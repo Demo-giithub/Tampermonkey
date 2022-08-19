9e2290194ae3df8652ace1baf045d75053fe5487
menglingwu <menglingwu@pansoft.com>
2022-08-20 00:23:14 +0800

    油猴脚本模板

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://mail.qq.com/cgi-bin/frame_html?sid=QQtpsPhT7CnWmwxu&r=815d7e51d2e210c1f39cb94ef4a87a63&t=newwin_frame&url=%2Fcgi-bin%2Freadmail%3Fmailid%3DZC0019_0JrNbyeMFNEu9WIAjBhv0c8%26need_textcontent%3Dtrue%26s%3Dnotify%26newwin%3Dtrue%26t%3Dreadmail&lang=zh
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=qq.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
