1ad9455d858894421152d3c90b9f40ac814cf510
menglingwu <menglingwu@pansoft.com>
2022-08-20 13:47:19 +0800

    create Script

--- temp.js
+++ temp.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Hello World
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
-// @match        https://mail.qq.com/cgi-bin/frame_html?sid=QQtpsPhT7CnWmwxu&r=815d7e51d2e210c1f39cb94ef4a87a63&t=newwin_frame&url=%2Fcgi-bin%2Freadmail%3Fmailid%3DZC0019_0JrNbyeMFNEu9WIAjBhv0c8%26need_textcontent%3Dtrue%26s%3Dnotify%26newwin%3Dtrue%26t%3Dreadmail&lang=zh
+// @author       meng
+// @match        *://*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=qq.com
 // @grant        none
 // ==/UserScript==
@@ -12,5 +12,5 @@
 (function() {
     'use strict';
 
-    // Your code here...
+    console.log("Hello World");
 })();
